c517220960cfc05da11969c4b103b965f4e47266
Fan <theFRP@gmail.com>
2015-06-20 17:14:18 +0800

    finally

--- themes/mobile/views/cart/success.php
+++ themes/mobile/views/cart/success.php
@@ -1,11 +1,14 @@
 <div class="content">
 <?php if($model['pay_style'] == '0'):?>
 
-订单提交成功，请尽快支付<?php echo $model['amount'];?>元,我们会尽快把外卖送到<?php echo $model['address']?>,联系电话:<?php echo $model['uid']?>.
+订单提交成功，请尽快支付<?php echo $model['amount'];?>元,我们会用最快速度把外卖送到<?php echo $model['address']?>,你的电话:<?php echo $model['uid']?>.
 <?php else:?>
 
-订单提交成功，请准备好<?php echo $model['amount'];?>元,我们会尽快把外卖送到<?php echo $model['address']?>,联系电话:<?php echo $model['uid']?>.
+订单提交成功，请准备好<?php echo $model['amount'];?>元,我们会尽快把外卖送到<?php echo $model['address']?>,你的电话:<?php echo $model['uid']?>.
 
 <?php endif;?>
+<br/>
+
+点击右上角的电话图标可以直接联系我们，进行取消订单等操作.
 
 </div>
